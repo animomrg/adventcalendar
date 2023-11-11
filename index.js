a96e8a19929fef5716c7e82702ee01224897c8a5
animomrg <143286196+animomrg@users.noreply.github.com>
2023-11-11 14:19:21 -0800

    first sync of advent calendar files

---
+++ index.js
@@ -0,0 +1,161 @@
+// variables for main calendar page
+const dayBtns = document.querySelectorAll('.btn-day');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+// add event listener to toggle modal on day click
+dayBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        modalOverlay.classList.add('open-modal');
+        updateSlide();
+    });
+});
+
+// variables for slides
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const closeBtn = document.querySelector('.close-btn');
+const slides = [
+    {
+        header: 'Happy December 1!',
+        text: "As always, the best thing about turning over the calendar to December is opening the first door of the advent calendar!",
+    },
+    {
+        header: 'Instructions',
+        text: "Each day, when you come back to your advent calendar and open the day's door, you'll find five trivia questions waiting for you.",
+    },
+    {
+        header: 'Instructions',
+        text: "Click on the best answer from the multiple-choice selections in order to pull up the next question. Time matters so once you know you're answer be sure to quickly choose the correct option.",
+    },
+    {
+        header: 'Ready?',
+        text: "Without further ado, let's get started!",
+    },
+];
+const slideHeader = document.getElementById('slide-header');
+const slideText = document.getElementById('slide-text');
+const playBtn = document.querySelector('.play-btn');
+const modalContainer = document.querySelector('.modal-container');
+
+let currentSlide = 0;
+
+function updateSlide() {
+    const slide = slides[currentSlide];
+    slideHeader.textContent = slide.header;
+    slideText.textContent = slide.text;
+    if (currentSlide < slides.length - 1) {
+        nextBtn.style.display = 'inline';
+    } else {
+        nextBtn.style.display = 'none';
+    }
+
+    if (currentSlide > 0) {
+        prevBtn.style.display = 'inline';
+    } else {
+        prevBtn.style.display = 'none';
+    }
+
+    if (currentSlide === slides.length - 1) {
+        playBtn.style.display = 'inline';
+    } else {
+        playBtn.style.display = 'none';
+    }
+};
+
+nextBtn.addEventListener('click', function() {
+    currentSlide++;
+    updateSlide();
+});
+
+prevBtn.addEventListener('click', function() {
+    currentSlide--;
+    updateSlide();
+});
+
+closeBtn.addEventListener('click', function() {
+    modalOverlay.classList.remove('open-modal');
+    currentSlide = 0;
+});
+
+playBtn.addEventListener('click', function() {
+    modalContainer.innerHTML = "<h1>Trivia!</h1>"
+
+})
+
+
+// COLOR THEMES AND TOGGLING //
+const redToggle = document.getElementById('red-theme');
+const snowToggle = document.getElementById('snow-theme');
+const greenToggle = document.getElementById('green-theme');
+
+let redStyle = true;
+let greenStyle = false;
+let snowStyle = false;
+
+const root = document.querySelector(':root');
+
+function greenTheme() {
+    root.style.setProperty('--clr-primary-1', '#00DB1D');
+    root.style.setProperty('--clr-primary-2', '#22A834');
+    root.style.setProperty('--clr-primary-3', '#2F7538');
+    root.style.setProperty('--clr-primary-4', '#124819');
+    root.style.setProperty('--clr-primary-5', '#29332A');
+}
+
+function redTheme() {
+    root.style.setProperty('--clr-primary-1', '#DB0000');
+    root.style.setProperty('--clr-primary-2', '#A82222');
+    root.style.setProperty('--clr-primary-3', '#752F2F');
+    root.style.setProperty('--clr-primary-4', '#4b0f0f');
+    root.style.setProperty('--clr-primary-5', '#331010');    
+}
+
+function snowTheme() {
+    root.style.setProperty('--clr-primary-1', '#CDFFFF');
+    root.style.setProperty('--clr-primary-2', '#A6FFFF');
+    root.style.setProperty('--clr-primary-3', '#00FFFF');
+    root.style.setProperty('--clr-primary-4', '#02E6E6');
+    root.style.setProperty('--clr-primary-5', '#6BC8FF');    
+}
+
+redToggle.addEventListener('click', function() {
+    if (!redStyle) {
+        redTheme();
+        redStyle = true;
+        greenStyle = false;
+        snowStyle = false;
+    }
+});
+
+snowToggle.addEventListener('click', function() {
+    if (!snowStyle) {
+        snowTheme();
+        redStyle = false;
+        greenStyle = false;
+        snowStyle = true;
+    }
+});
+
+greenToggle.addEventListener('click', function() {
+    if (!greenStyle) {
+        greenTheme();
+        redStyle = false;
+        greenStyle = true;
+        snowStyle = false;
+    }
+});
+
+/*/ FONT TOGGLING /*/
+
+const fonts = ['Mountains of Christmas', 'Comic Neue', 'St Nicholas', 'Montserrat'];
+const fontToggle = document.getElementById('font-btn');
+let fontIndex = 0;
+
+fontToggle.addEventListener('click', function() {
+    if (fontIndex === fonts.length - 1) {
+        fontIndex = 0;
+    } else {
+        fontIndex += 1;
+    }
+    root.style.setProperty('--ff-primary', fonts[fontIndex]);
+});
